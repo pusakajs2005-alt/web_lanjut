8b8f55b3c3bf5591e28ebed7a3f1fff176c6aa56
Pusaka Jaya S. <pusaka.js2005@gmail.com>
2026-06-03 11:35:10 +0700

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,14 @@
  import { Sequelize } from "sequelize";
 import mysql2 from 'mysql2';
- const db = new Sequelize('perpustakaan', 'root', '', {
-     host: "localhost",
+ const db = new Sequelize('web_lanjut', 'avnadmin', 'AVNS_8FZj8Wqs6QbEvxD11nw', {
+     host: "mysql-c850987-weblanjut1.h.aivencloud.com",
      dialect: "mysql",
+     port: 25801,
+     dialectOptions: { 
+        ssl: { 
+        rejectUnauthorized: false 
+       } 
+    }, 
       "define": {
         "timestamps": false
       }
