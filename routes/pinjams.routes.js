5f2d23ac43e6561340e38ebbb5f43c718073debb
Pusaka Jaya S. <pusaka.js2005@gmail.com>
2026-06-10 07:53:00 +0700

    ok

--- routes/pinjams.routes.js
+++ routes/pinjams.routes.js
@@ -7,14 +7,14 @@ import {
     insertPinjam,
 } from "../controllers/pinjams.controllers.js";
 
-import { authenticateToken } from "../middleware/VerifyTokens.js";
+/*import { authenticateToken } from "../middleware/VerifyTokens.js";*/
 const router = express.Router();
-router.get("/", authenticateToken, getAllPinjam);
+router.get("/",  getAllPinjam);
 //router.post("/", tambahPinjambaru);
-router.get("/nim/:id", authenticateToken, cariPinjamByid);
+router.get("/nim/:id",  cariPinjamByid);
 //router.patch("/:id", updatePinjam);
-router.delete("/:id", authenticateToken, deletePinjam);
-router.post("/", authenticateToken, insertPinjam);
+router.delete("/:id",  deletePinjam);
+router.post("/",  insertPinjam);
 //router.patch("/pengembalian/:id", updatePengembalian);
 
 
